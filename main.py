2b5c5763367738e9d596a0c3cb2967136f74652c
PinkyOSRS <138986495+PinkyOSRS@users.noreply.github.com>
2025-05-29 20:46:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,6 @@ async def on_ready():
     await client.close()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
